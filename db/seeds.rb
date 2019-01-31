442213162c90ce06d84d566ab1b75b525a674d76
Samkttn <kettani.samir@outlook.fr>
2019-01-31 17:07:08 +0100

    first init

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+require 'faker'
+
+10.times do 
+    doctor = Doctor.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, zip_code: Faker::Address.zip_code)
+end
+
+100.times do
+    patient = Patient.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+end
+
+120.times do
+    appointment = Appointment.create!(date: Faker::)
